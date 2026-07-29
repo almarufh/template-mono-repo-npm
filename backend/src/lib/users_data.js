3f17ceccdc053033f65aa01e0eba97a339c4a87e
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-29 11:05:43 +0800

    fix: delete constatn FILE_PATH use param because many path

--- backend/src/lib/users_data.js
+++ backend/src/lib/users_data.js
@@ -1,36 +1,52 @@
 import fs from 'fs';
 import path from 'path';
 
-const FILE_PATH = path.join(process.cwd(), 'backend/data', 'users.json');
-
 function directory(filePath) {
-    const dirname = path.dirname(filePath);
-    if (!fs.existsSync(dirname)) {
-        fs.mkdirSync(dirname, { recursive: true });
-    }
+
+   const dirname = path.dirname(filePath);
+   if (!fs.existsSync(dirname)) {
+
+      fs.mkdirSync(dirname, { recursive: true });
+   
+   }
+
 }
 
-export function loadData() {
-    try {
-        directory(FILE_PATH);
-        if (!fs.existsSync(FILE_PATH)) {
-            const initialData = [];
-            fs.writeFileSync(FILE_PATH, JSON.stringify(initialData, null, 2), 'utf-8');
-            return initialData;
-        }
-        const fileContent = fs.readFileSync(FILE_PATH, 'utf-8');
-        return JSON.parse(fileContent);
-    } catch (error) {
-        console.error('[LOAD FILE ERROR]', error.message);
-        return [];
-    }
+export function loadData(FILE_PATH) {
+
+   try {
+
+      directory(FILE_PATH);
+      if (!fs.existsSync(FILE_PATH)) {
+
+         const initialData = [];
+         fs.writeFileSync(FILE_PATH, JSON.stringify(initialData, null, 2), 'utf-8');
+         return initialData;
+      
+      }
+      const fileContent = fs.readFileSync(FILE_PATH, 'utf-8');
+      return JSON.parse(fileContent);
+   
+   } catch (error) {
+
+      console.error('[LOAD FILE ERROR]', error.message);
+      return [];
+   
+   }
+
 }
 
-export function saveData(data) {
-    try {
-        directory(FILE_PATH);
-        fs.writeFileSync(FILE_PATH, JSON.stringify(data, null, 2), 'utf-8');
-    } catch (error) {
-        console.error('[FILE SAVE ERROR]', error.message);
-    }
+export function saveData(data, FILE_PATH) {
+
+   try {
+
+      directory(FILE_PATH);
+      fs.writeFileSync(FILE_PATH, JSON.stringify(data, null, 2), 'utf-8');
+   
+   } catch (error) {
+
+      console.error('[FILE SAVE ERROR]', error.message);
+   
+   }
+
 }
