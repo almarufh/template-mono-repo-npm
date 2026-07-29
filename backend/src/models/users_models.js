ba89ab8821c94bfcf7a370fbfbf7af2a5a2fc787
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-29 12:29:11 +0800

    fix: name path for use others file

--- backend/src/models/users_models.js
+++ backend/src/models/users_models.js
@@ -5,72 +5,88 @@ import {
    saveData as save  
 } from "../lib/users_data.js";
 
-const FILE_PATH = path.join(process.cwd(), 'backend/data', 'users.json');
+const PATH_USERS = path.join(process.cwd(), 'backend/data', 'users.json');
 
 function createUser ( newUser ) {
-    let users = load(FILE_PATH);
-    users.push(newUser)
-    save(users)
-    return newUser
+
+   let users = load(PATH_USERS);
+   console.log("users", users);
+   users.push(newUser);
+   save(users, PATH_USERS);
+   return newUser;
+
 }
 
 function findUserByEmail (email) {
-    let users = load(FILE_PATH);
-    const results = users.find(u => u.email === email)
-    if (results === undefined) {
-        return {
-            success: false,
-            message:"User not found",
-            data: results || null
-        }
-    } else {
-        return {
-            succes: true,
-            message: `Found user ${results.name}`,
-            data: results
-        }
-    }
+
+   let users = load(PATH_USERS);
+   const results = users.find(u => u.email === email);
+   if (results === undefined) {
+
+      return {
+         success: false,
+         message:"User not found",
+         data: results || null
+      };
+   
+   } else {
+
+      return {
+         succes: true,
+         message: `Found user ${results.name}`,
+         data: results
+      };
+   
+   }
+
 }
 
 function findUserById (id) {
-    let users = load(FILE_PATH);
-    const results = users.find(u => u.id === id)
-    if (results === undefined) {
-        return {
-            success: false,
-            message:"User not found",
-            data: results || null
-        }
-    } else {
-        return {
-            succes: true,
-            message: `Found user ${results.name}`,
-            data: results
-        }
-    }
+
+   let users = load(PATH_USERS);
+   const results = users.find(u => u.id === id);
+   if (results === undefined) {
+
+      return {
+         success: false,
+         message:"User not found",
+         data: results || null
+      };
+   
+   } else {
+
+      return {
+         succes: true,
+         message: `Found user ${results.name}`,
+         data: results
+      };
+   
+   }
+
 }
 
 
+let newUser = {
+   "id": 1,
+   "name": "Alhy Ghufron",
+   "email": "alhy23@gmail.com",
+   "password": "1234"
+};
+
 // let newUser = {
-//     "id": 1,
-//     "name": "Alhy Ghufron",
-//     "email": "alhy23@gmail.com",
+//     "id": 2,
+//     "name": "Alma'ruf Hidayat",
+//     "email": "hidayatmaruf99@gmail.com",
 //     "password": "1234"
 // }
 
-let newUser = {
-    "id": 2,
-    "name": "Alma'ruf Hidayat",
-    "email": "hidayatmaruf99@gmail.com",
-    "password": "1234"
-}
-
 export {
-    newUser,
-    createUser,
-    findUserByEmail,
-    findUserById
-}
+   newUser,
+   PATH_USERS,
+   createUser,
+   findUserByEmail,
+   findUserById
+};
 
 // console.log(createUser(newUser))
 // console.log(findUserByEmail("alhy23@gmail.com"))
