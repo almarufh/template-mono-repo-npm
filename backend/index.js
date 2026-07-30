30a7ee735fa7c08bb27b9e22dc3a59813ca546f5
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-30 14:05:11 +0800

    feat: setup option swagerJsDocs and serve swaggerUi

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,31 @@
 import express from 'express';
+import swagerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 import routers from './src/routers/index.js';
 
 const PORT = process.env.PORT;
 const app = express();
 
-app.use(express.urlencoded());
+// swagger
+const options = {
+   definition: {
+      openapi: '3.0.0',
+      info: {
+         title: 'KeyNotes',
+         version: '1.0.0',
+      }
+   },
+   apis: ['./src/routers/*_routers.js'],
+}
+const openapi = swaggerJSDoc(options)
+
+
+// REST API
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routers);
 
+app.use('/docs', swagerUi.serve, swagerUi.setup(openapi))
 
 app.listen(PORT, () => {
 
