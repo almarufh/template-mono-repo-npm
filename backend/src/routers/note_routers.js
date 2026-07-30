d2e0fd60a4ea19e24dfbbbed970eafad4604d7b6
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-30 14:06:07 +0800

    feat: jsdoc to converrt swaggerui

--- backend/src/routers/note_routers.js
+++ backend/src/routers/note_routers.js
@@ -2,9 +2,163 @@ import { Router } from "express";
 import * as cNote from "../controllers/notes_controllers.js";
 
 const notesRouter = Router();
-
+/**
+ * @openapi
+ * /notes/create:
+ *  post:
+ *    tags:
+ *      - Notes
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              id:
+ *                type: string
+ *                example: 1785386817692
+ *              user:
+ *                type: string
+ *                example: 1785312952021
+ *              title:
+ *                type: string
+ *                example: Everything is Mindset
+ *              content:
+ *                type: string
+ *                example: yang akan kita dapatkan bukanlah apa yang kita mimpikan tetapi apa yang kita lakukan
+ *              label:
+ *                type: string
+ *                example: mindset
+ *              pin:
+ *                type: boolean
+ *                example: false
+ *              arch:
+ *                type: boolean
+ *                example: false
+ *              del:
+ *                type: boolean
+ *                example: true
+ *        application/x-www-form-urlencoded:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              id:
+ *                type: string
+ *                example: 1785386817692
+ *              user:
+ *                type: string
+ *                example: 1785312952021
+ *              title:
+ *                type: string
+ *                example: Everything is Mindset
+ *              content:
+ *                type: string
+ *                example: yang akan kita dapatkan bukanlah apa yang kita mimpikan tetapi apa yang kita lakukan
+ *              label:
+ *                type: string
+ *                example: mindset
+ *              pin:
+ *                type: boolean
+ *                example: false
+ *              arch:
+ *                type: boolean
+ *                example: false
+ *              del:
+ *                type: boolean
+ *                example: true
+ *    responses:
+ *      '200':
+ *        description: logout successs
+ */
 notesRouter.post("/create", cNote.createNotes);
+/**
+ * @openapi
+ * /notes/change:
+ *  PATCH:
+ *    tags:
+ *      - Notes
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              id:
+ *                type: string
+ *                example: 1785386817692
+ *              user:
+ *                type: string
+ *                example: 1785312952021
+ *              title:
+ *                type: string
+ *                example: Everything is Mindset
+ *              content:
+ *                type: string
+ *                example: yang akan kita dapatkan bukanlah apa yang kita mimpikan tetapi apa yang kita lakukan
+ *              label:
+ *                type: string
+ *                example: mindset
+ *              pin:
+ *                type: boolean
+ *                example: false
+ *              arch:
+ *                type: boolean
+ *                example: false
+ *              del:
+ *                type: boolean
+ *                example: true
+ *        application/x-www-form-urlencoded:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              id:
+ *                type: string
+ *                example: 1785386817692
+ *              user:
+ *                type: string
+ *                example: 1785312952021
+ *              title:
+ *                type: string
+ *                example: Everything is Mindset
+ *              content:
+ *                type: string
+ *                example: yang akan kita dapatkan bukanlah apa yang kita mimpikan tetapi apa yang kita lakukan
+ *              label:
+ *                type: string
+ *                example: mindset
+ *              pin:
+ *                type: boolean
+ *                example: false
+ *              arch:
+ *                type: boolean
+ *                example: false
+ *              del:
+ *                type: boolean
+ *                example: true
+ *    responses:
+ *      '200':
+ *        description: logout successs
+ */
 notesRouter.patch("/change", cNote.changeNotes);
+
+
+/**
+ * @openapi
+ * /notes/:
+ *  get:
+ *    tags:
+ *      - Notes
+ *    parameters:
+ *      - name: id
+ *        in: query
+ *        required: true
+ *        schema:
+ *          type: string
+ *          example: 1785312952021
+ *    responses:
+ *      '200':
+ *        description: logout successs
+ */
 notesRouter.get("/:id", cNote.getNotesUser);
 
 export default notesRouter;
