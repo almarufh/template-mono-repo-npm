b3efad7decdb9834c1828c4e8620b0ee65b677b5
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-30 14:06:23 +0800

    feat: jsdoc to converrt swaggerui

--- backend/src/routers/auth_routers.js
+++ backend/src/routers/auth_routers.js
@@ -3,8 +3,104 @@ import * as cAuth from "../controllers/auth_controllers.js";
 
 const authRouter = Router();
 
+/**
+ * @openapi
+ * /auth/register:
+ *  post:
+ *    tags:
+ *      - Auth
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                example: admin@gmail.com
+ *              password:
+ *                type: string
+ *                example: 1234
+ *              name:
+ *                type: string
+ *                example: admin
+ *        application/x-www-form-urlencoded:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                example: admin@gmail.com
+ *              password:
+ *                type: string
+ *                example: 1234
+ *              name:
+ *                type: string
+ *                example: admin
+ *    responses:
+ *      '200':
+ *        description: register successs
+ */
 authRouter.post("/register", cAuth.register);
+/**
+ * @openapi
+ * /auth/login:
+ *  post:
+ *    tags:
+ *      - Auth
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                example: admin@gmail.com
+ *              password:
+ *                type: string
+ *                example: 1234
+ *        application/x-www-form-urlencoded:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                example: admin@gmail.com
+ *              password:
+ *                type: string
+ *                example: 1234
+ *    responses:
+ *      '200':
+ *        description: login successs
+ */
 authRouter.post("/login", cAuth.login);
+/**
+ * @openapi
+ * /auth/logout:
+ *  post:
+ *    tags:
+ *      - Auth
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              id:
+ *                type: string
+ *                example: 1785312447951
+ *        application/x-www-form-urlencoded:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              id:
+ *                type: string
+ *                example: 1785312447951
+ *    responses:
+ *      '200':
+ *        description: logout successs
+ */
 authRouter.post("/logout", cAuth.logout);
 
 export default authRouter;
